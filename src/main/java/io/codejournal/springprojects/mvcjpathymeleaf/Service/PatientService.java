d549f6cd4432bf37151fb5435a29131acc8dc7f1
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-06-20 23:46:25 -0500

    add new patient

---
+++ src/main/java/io/codejournal/springprojects/mvcjpathymeleaf/Service/PatientService.java
@@ -0,0 +1,10 @@
+package io.codejournal.springprojects.mvcjpathymeleaf.Service;
+
+import io.codejournal.springprojects.mvcjpathymeleaf.dto.AppointmentResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface TestService {
+}
